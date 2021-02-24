c12c2b4895f39cafc4d232c5eff25506336056f6
Ivan Gonzalez Ibarra <ivan.gonzalez@liveviewtech.com>
2021-02-23 20:44:32 -0700

    Refactored database into extensions.py and authorization views into views/auth_views.py as a blueprint instead of main application.

--- tests/test_extensions.py
+++ tests/test_extensions.py
@@ -1,5 +1,38 @@
+import pytest
 from api.extensions import Role, User, db
 
 
-def test_placeholder():
-    pass
+usernames = [
+    "apple", "banana", "cherry", "dragon fruit", "egg fruit", "Farkleberry", "grapefruit",
+    "honeydewmelon", "Indonesian Lime", "Jackfruit"
+]
+emails = [
+    'a@a.com', 'b@b.com', 'c@c.com', 'd@d.com', 'e@e.com', 'f@f.com', 'g@g.com', 'h@h.com',
+    'i@i.com', 'j@j.com'
+]
+first_names = [
+    'Bob', 'Chad', 'Spok', 'Elmo', 'Twee', 'Dee', 'Ainz', 'ConAir', 'Winston', 'Who'
+]
+last_names = [
+    'Whitaker', 'Bolton', 'Vang', 'Mosley', 'Faulkner', 'Kline', 'Farmer', 'Short',
+    'Huerta', 'Osborn'
+]
+passwords = [
+    'password', 'asdf', '1qaz!QAZ2wsx@WSX', 'abcd1234', 'ConnorWithAnEIsLame', 'ConnorWithAnOIsCooler', 'Woooooooooooo',
+    'Short', 'Huerta', 'Osborn'
+]
+
+
+@pytest.mark.parametrize('email, firstname, lastname, username, passwords', [usernames, emails, first_names, last_names, passwords])
+def test_create_users(email, firstname, lastname, username):
+    if not User.query.filter(User.email == 'member@example.com').first():
+        user = User(
+            email=email,
+            firstname=firstname,
+            lastname=lastname,
+            username=username
+        )
+        user.hash_password('blue')
+        user.roles.append(Role(name='member'))
+        db.session.add(user)
+        db.session.commit()
